0e05c9ffa7ec73807dc393a32e254e8b53f35f4b
Rafid Aslam (refeed) <rafidteam@gmail.com>
2023-05-11 17:12:58 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_s3_bucket" "example" { }
+resource "aws_s3_bucket" "another_s3" {}
+
+resource "aws_security_group" "allow_tls" {
+}
+
+resource "aws_instance" "example_c" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  depends_on = [aws_s3_bucket.example]
+}
+
+module "example_sqs_queue" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "3.3.0"
+
+  depends_on = [aws_s3_bucket.example, aws_instance.example_c]
+}
+
+
